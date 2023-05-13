516126c81c5ef8a0f4418a2c7f90c580f2c19d77
Freddie Pankhurst <fpa34@uclive.ac.nz>
2023-05-13 14:45:51 +1200

    Implement desired altitude and yaw calcs, and stoped floating point error in controler

--- control.c
+++ control.c
@@ -2,10 +2,10 @@
 
 static uint8_t  control_update_freq; // Check back on this <----------------
 
-static uint32_t previous_altitude_error = 0;
+static uint16_t previous_altitude_error = 0;
 static uint32_t sum_altitude_error = 0;
 
-static uint32_t previous_yaw_error = 0;
+static uint16_t previous_yaw_error = 0;
 static uint32_t sum_yaw_error = 0;
 
 void calculateAltitudeControl(void);
@@ -25,17 +25,18 @@ void updateControl(void)
 
 void calculateAltitudeControl(void)
 {
-    uint32_t error = getAltitudeError();
+    uint16_t error = getAltitudeError();
 
-    uint32_t derivative_error = (error - previous_altitude_error) * control_update_freq;
-    
-    uint32_t temp_errror_sum = sum_altitude_error += error;
+    uint32_t proporional_error = error * 100;
+    uint32_t derivative_error = (error - previous_yaw_error) * control_update_freq * 100;
+    uint32_t intergral_error_sum = sum_yaw_error + (error * 100) / control_update_freq;
 
-    uint32_t control_output = ALTITUDE_KP * error + ALTITUDE_KD * derivative_error + ALTITUDE_KI * sum_altitude_error;
+    uint32_t control_output = YAW_KP * proporional_error + YAW_KD * derivative_error + YAW_KI * intergral_error_sum;
+    control_output /= 100;
 
     if (control_output <= PWM_MAX_DUTY_CYCLE && control_output >= PWM_MIN_DUTY_CYCLE)
     {
-        sum_altitude_error += temp_errror_sum;
+        sum_altitude_error += intergral_error_sum;
     }
 
     previous_altitude_error = error;
@@ -46,17 +47,18 @@ void calculateAltitudeControl(void)
 
 void calculateYawControl(void)
 {
-    uint32_t error = getYawError();
+    uint16_t error = getYawError();
 
-    uint32_t derivative_error = (error - previous_yaw_error) * control_update_freq;
-    
-    uint32_t temp_errror_sum = sum_yaw_error + error / control_update_freq;
+    uint32_t proporional_error = error * 100;
+    uint32_t derivative_error = (error - previous_yaw_error) * control_update_freq * 100;
+    uint32_t intergral_error_sum = sum_yaw_error + (error * 100) / control_update_freq;
 
-    uint32_t control_output = YAW_KP * error + YAW_KD * derivative_error + YAW_KI * sum_yaw_error;
+    uint32_t control_output = YAW_KP * proporional_error + YAW_KD * derivative_error + YAW_KI * intergral_error_sum;
+    control_output /= 100;
 
     if (control_output <= PWM_MAX_DUTY_CYCLE && control_output >= PWM_MIN_DUTY_CYCLE)
     {
-        sum_yaw_error += temp_errror_sum;
+        sum_yaw_error += intergral_error_sum;
     }
 
     previous_yaw_error = error;
