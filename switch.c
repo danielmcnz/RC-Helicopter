151850d5e16de5f875b8ff06905015916acc5aa1
Freddie Pankhurst <fpa34@uclive.ac.nz>
2023-05-13 17:55:26 +1200

    Added Switch Functionality

---
+++ switch.c
@@ -0,0 +1,44 @@
+#include "switch.h"
+
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+
+#define PERIPH_SWITCH_1     SYSCTL_PERIPH_GPIOA
+#define PORT_BASE_SWITCH_1  GPIO_PORTA_BASE
+#define PIN_SWITCH_1        GPIO_PIN_7
+
+static switch_t switch_1;
+
+void initSwitch(void)
+{
+    SysCtlPeripheralEnable(PERIPH_SWITCH_1);
+    GPIOPinTypeGPIOInput(PORT_BASE_SWITCH_1, PIN_SWITCH_1);
+    GPIOPadConfigSet(PORT_BASE_SWITCH_1, PIN_SWITCH_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPD);
+    switch_1.switch_position = GPIOPinRead(PORT_BASE_SWITCH_1, PIN_SWITCH_1) == PIN_SWITCH_1;
+}
+
+void checkSwitch(void)
+{
+    switch_1.check_switch_position = GPIOPinRead(PORT_BASE_SWITCH_1, PIN_SWITCH_1) == PIN_SWITCH_1;
+
+    if (switch_1.check_switch_position != switch_1.switch_position)
+    {
+        switch_1.switch_changed = true;
+    }
+    switch_1.switch_position = switch_1.check_switch_position;
+}
+
+switch_state_t getSwitch(void)
+{
+    if (switch_1.switch_changed)
+    {
+        switch_1.switch_changed = false;
+
+        if (switch_1.switch_position)
+        {
+            return Switch_Active
+        }
+        return Switch_Deactive
+    }
+    return Switch_Same_State
+}
---
+++ switch.h
@@ -0,0 +1,24 @@
+#ifndef SWITCH_H_
+#define SWITCH_H_
+
+#include <stdbool.h>
+
+typedef enum {
+    Switch_Same_State = 0,
+    Switch_Active,
+    Switch_Deactive
+} switch_state_t;
+
+typedef struct
+{
+    bool switch_position;
+    bool check_switch_position;
+    bool switch_changed;
+} switch_t;
+
+
+void initSwitch(void);
+void checkSwitch(void);
+switch_state_t getSwitch(void);
+
+#endif
