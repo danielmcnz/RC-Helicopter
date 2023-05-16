307fceb9f9ad8a35097cdad13594d3eae2dc96da
FJGP48 <90247188+FJGP48@users.noreply.github.com>
2023-05-16 17:17:54 +1200

    Code Comments (Yaw)

--- yaw.c
+++ yaw.c
@@ -1,9 +1,13 @@
-/*
- * Yaw.cpp
- *
- *  Created on: 3/05/2023
- *      Author: dmc270
- */
+//**********************************************************
+// File: yaw.c
+//
+// Authors: Freddie Pankhurst   (fpa34)
+//          Daniel McGregor     (dmc270)
+//
+// Handles reading and calculating yaw and determining
+// the rotation of the helicopter
+//
+//**********************************************************
 
 #include "yaw.h"
 #include "heliState.h"
@@ -14,13 +18,28 @@
 #include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
 
+//**********************************************************
+// static variables
+//**********************************************************
+
+// Current yaw calcualted from a zeroed position, (+ve or -ve)
 static int16_t yaw;
-static YawPosition yaw_pos;
 
-static bool yawRef = false;
-
+// Desired rotation (where it should be), used to calcuate the error and
+// thus the PID cotnrol, can be incremented and decremented
 static int16_t desired_yaw = 0;
 
+// Struct containing the degree and sub degree for the rotation of the heli
+static YawPosition yaw_pos;
+
+// Boolean set true is the yawRef interupt handler is triggered during
+// the taking off state
+static bool yawRef = false;
+
+//*****************************************************************************
+// YawRefIntHandler: handler for the yawRef interupt, resets yaw and desired
+// position to the current location at time of interupt
+//*****************************************************************************
 void YawRefIntHandler(void)
 {
     GPIOIntClear(GPIO_PORTC_BASE, GPIO_PIN_4);
@@ -33,6 +52,10 @@ void YawRefIntHandler(void)
     }
 }
 
+//*****************************************************************************
+// YawIntHandler: handler for the pin interrupts, used in the decoder to
+// determine rotion and direction
+//*****************************************************************************
 void YawIntHandler(void)
 {
     static int prevState = INVALID;
@@ -59,7 +82,7 @@ void YawIntHandler(void)
 
     prevState = signalAB;
 
-    // ensures yaw value does not overflow integer type
+    // bound yaw to +=448 (max yaw increments in 360 deg)
     if(yaw > N_SLOTS)
     {
         yaw -= N_SLOTS;
@@ -71,10 +94,13 @@ void YawIntHandler(void)
     GPIOIntClear(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 }
 
+//**********************************************************
+// initYaw: initialize the YawRefIntHandler and
+// YawIntHandler pin interupts
+//**********************************************************
 void initYaw(void)
 {
     // initialize yaw A and B
-
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 
     GPIOPinTypeGPIOInput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
@@ -85,7 +111,6 @@ void initYaw(void)
     GPIOIntEnable(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
     // initialize yaw reference
-
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
 
     GPIOIntDisable(GPIO_PORTC_BASE, GPIO_PIN_4);
@@ -103,11 +128,13 @@ void initYaw(void)
 
     GPIOIntEnable(GPIO_PORTC_BASE, GPIO_PIN_4);
 
-    // GPIOIntClear(GPIO_PORTC_BASE, GPIO_PIN_4);
-
     IntEnable(INT_GPIOC);
 }
 
+//**********************************************************
+// boundYawDeg: bounds an int16_t in degrees to between
+// the MAX and MIN rotation (-179.9->180)
+//**********************************************************
 int16_t boundYawDeg(int16_t boundingVar)
 {
     if((boundingVar > MAX_YAW))
@@ -122,19 +149,21 @@ int16_t boundYawDeg(int16_t boundingVar)
     return boundingVar;
 }
 
+// **********************************************************
+// updateYaw: updates the yaw_pos values by converting the
+// yaw into degrees
+// **********************************************************
 void updateYaw(void)
 {
     // convert yaw to a range between 0 to 360 degrees from gpio value
-    yaw_pos.degree = ((yaw * FULL_ROTATION) / N_SLOTS) % FULL_ROTATION; // degree precision
-    // yaw_pos.sub_degree = ((yaw * 8) % FULL_ROTATION) % 10; // sub degree precision
+    // degree precision
+    yaw_pos.degree = ((yaw * FULL_ROTATION) / N_SLOTS) % FULL_ROTATION;
+    // subdegree precision
     yaw_pos.sub_degree = ((yaw * DECIMAL_PLACES * FULL_ROTATION) / N_SLOTS) % DECIMAL_PLACES;
 
     // constrain yaw sub precision to be positive as sign is already handled by degree calculation
     if(yaw_pos.sub_degree < 0)
         yaw_pos.sub_degree = -yaw_pos.sub_degree;
-
-    // constrain yaw between -180 to 180 degrees
-//    yaw_pos.degree = boundYawDeg(yaw_pos.degree);
 
     if((yaw_pos.degree > MAX_YAW))
     {
@@ -146,16 +175,27 @@ void updateYaw(void)
     }
 }
 
+//**********************************************************
+// getYaw: Returns the stuct containing the current rotation
+// in degrees (including subdegree)
+//**********************************************************
 YawPosition getYaw(void)
 {
     return yaw_pos;
 }
 
+//**********************************************************
+// resetDesiredYaw: Resets the desired yaw to 0
+//**********************************************************
 void resetDesiredYaw(void)
 {
     desired_yaw = 0;
 }
 
+//**********************************************************
+// incrementYaw: Increases the desired yaw by YAW_INCREMENT
+// and then rebounds it back to +-180
+//**********************************************************
 void incrementYaw(void)
 {
     desired_yaw += YAW_INCREMENT;
@@ -163,6 +203,10 @@ void incrementYaw(void)
     desired_yaw = boundYawDeg(desired_yaw);
 }
 
+//**********************************************************
+// decrementYaw: Decreases the desired yaw by YAW_INCREMENT
+// and then rebounds it back to +-180
+//**********************************************************
 void decrementYaw(void)
 {
     desired_yaw -= YAW_INCREMENT;
@@ -170,11 +214,18 @@ void decrementYaw(void)
     desired_yaw = boundYawDeg(desired_yaw);
 }
 
+//**********************************************************
+// getDesiredYaw: Returns the desired yaw value
+//**********************************************************
 int16_t getDesiredYaw(void)
 {
     return desired_yaw;
 }
 
+//**********************************************************
+// getYawError: Returns current yaw error, between the
+// currrent position and the desired position
+//**********************************************************
 int16_t getYawError(void)
 {
     int16_t degree_error = getYaw().degree - getDesiredYaw();
@@ -182,16 +233,28 @@ int16_t getYawError(void)
     return degree_error;
 }
 
+//**********************************************************
+// getYawRef: Returns the falg signaling if the yawRef
+// interupt has been triggered
+//**********************************************************
 bool getYawRef(void)
 {
     return yawRef;
 }
 
+//**********************************************************
+// resetYawRef: Reset the yawRef flag back to false, so
+// taking off calcibration can be untertaken multiple times
+//**********************************************************
 void resetYawRef(void)
 {
     yawRef = false;
 }
 
+//**********************************************************
+// resetYaw: Resets the yaw, same as zeroing all rotation
+// about where you call resetYaw()
+//**********************************************************
 void resetYaw(void)
 {
     yaw = 0;
--- yaw.h
+++ yaw.h
@@ -1,9 +1,13 @@
-/*
- * Yaw.h
- *
- *  Created on: 3/05/2023
- *      Author: dmc270
- */
+//**********************************************************
+// File: yaw.h
+//
+// Authors: Freddie Pankhurst   (fpa34)
+//          Daniel McGregor     (dmc270)
+//
+// Handles reading and calculating yaw and determining
+// the rotation of the helicopter
+//
+//**********************************************************
 
 #ifndef YAW_H_
 #define YAW_H_
@@ -11,20 +15,30 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+//**********************************************************
+// Constant variables
+//**********************************************************
+
+// Bounding constants (also for some calcs)
 #define N_SLOTS 448
 #define FULL_ROTATION 360
 #define MAX_YAW 180
 #define MIN_YAW -180
+
+// Subdegree calculation
 #define DECIMAL_PLACES 10
+
+// Amount to inc/decrement desired yaw
 #define YAW_INCREMENT 15
 
+// Struct containing the degree and sub degree for the rotation of the heli
 typedef struct
 {
     int16_t degree;
     int8_t sub_degree;
 } YawPosition;
 
-// quadrature decoding states
+// Quadrature decoding states
 typedef enum {
     INVALID = -1,
     LO_LO,
@@ -33,24 +47,74 @@ typedef enum {
     HI_HI
 } QDStates;
 
+//**********************************************************
+// initYaw: initialize the YawRefIntHandler and
+// YawIntHandler pin interupts
+//**********************************************************
 void initYaw(void);
 
+// **********************************************************
+// updateYaw: updates the yaw_pos values by converting the
+// yaw into degrees
+// **********************************************************
 void updateYaw(void);
 
+//**********************************************************
+// boundYawDeg: bounds an int16_t in degrees to between
+// the MAX and MIN rotation (-179.9->180)
+//**********************************************************
 int16_t boundYawDeg(int16_t boundingVar);
 
+//**********************************************************
+// getYaw: Returns the stuct containing the current rotation
+// in degrees (including subdegree)
+//**********************************************************
 YawPosition getYaw(void);
 
+//**********************************************************
+// incrementYaw: Increases the desired yaw by YAW_INCREMENT
+// and then rebounds it back to +-180
+//**********************************************************
 void incrementYaw(void);
+
+//**********************************************************
+// decrementYaw: Decreases the desired yaw by YAW_INCREMENT
+// and then rebounds it back to +-180
+//**********************************************************
 void decrementYaw(void);
 
+//**********************************************************
+// getDesiredYaw: Returns the desired yaw value
+//**********************************************************
 int16_t getDesiredYaw(void);
+
+//**********************************************************
+// getYawError: Returns current yaw error, between the
+// currrent position and the desired position
+//**********************************************************
 int16_t getYawError(void);
 
+//**********************************************************
+// getYawRef: Returns the falg signaling if the yawRef
+// interupt has been triggered
+//**********************************************************
 bool getYawRef(void);
+
+//**********************************************************
+// resetYawRef: Reset the yawRef flag back to false, so
+// taking off calcibration can be untertaken multiple times
+//**********************************************************
 void resetYawRef(void);
+
+//**********************************************************
+// resetYaw: Resets the yaw, same as zeroing all rotation
+// about where you call resetYaw()
+//**********************************************************
 void resetYaw(void);
 
+//**********************************************************
+// resetDesiredYaw: Resets the desired yaw to 0
+//**********************************************************
 void resetDesiredYaw(void);
 
-#endif /* YAW_H_ */
+#endif // YAW_H_
