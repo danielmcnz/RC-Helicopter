86af292e4f8032815dd904cc6ffb693e33a66835
FJGP48 <90247188+FJGP48@users.noreply.github.com>
2023-05-16 16:42:09 +1200

    Code Comments (Switch)

--- switch.c
+++ switch.c
@@ -1,3 +1,14 @@
+//**********************************************************
+// File: switch.c
+
+// Authors: Freddie Pankhurst   (fpa34)
+//          Daniel McGregor     (dmc270)
+//
+// Support for switch input, for the Tiva board, handles
+// up and down pushes
+//
+//**********************************************************
+
 #include "switch.h"
 
 #include "inc/hw_memmap.h"
@@ -7,12 +18,25 @@
 #include "driverlib/debug.h"
 #include "inc/tm4c123gh6pm.h"
 
+//**********************************************************
+// Constants
+//**********************************************************
+
 #define PERIPH_SWITCH_1     SYSCTL_PERIPH_GPIOA
 #define PORT_BASE_SWITCH_1  GPIO_PORTA_BASE
 #define PIN_SWITCH_1        GPIO_PIN_7
 
+//**********************************************************
+// Static Variables
+//**********************************************************
+
+// Struct of current switch_1 position, new position, and
+// whether the switch has changed since last checked
 static switch_t switch_1;
 
+//**********************************************************
+// initSwitch: Initialises Switch_1
+//**********************************************************
 void initSwitch(void)
 {
     SysCtlPeripheralEnable(PERIPH_SWITCH_1);
@@ -22,6 +46,10 @@ void initSwitch(void)
     switch_1.switch_changed = false;
 }
 
+//**********************************************************
+// updateSwitch: Checks new switch position and sees
+// if switch_1's positions has changed
+//**********************************************************
 void updateSwitch(void)
 {
     switch_1.check_switch_position = GPIOPinRead(PORT_BASE_SWITCH_1, PIN_SWITCH_1) == PIN_SWITCH_1;
@@ -33,6 +61,10 @@ void updateSwitch(void)
     switch_1.switch_position = switch_1.check_switch_position;
 }
 
+//**********************************************************
+// checkSwitch: Checks switch_1 and returns if it has not
+// changed since last check, and then its positon
+//**********************************************************
 switch_state_t checkSwitch(void)
 {
     if (switch_1.switch_changed)
--- switch.h
+++ switch.h
@@ -1,15 +1,30 @@
+//**********************************************************
+// File: switch.h
+
+// Authors: Freddie Pankhurst   (fpa34)
+//          Daniel McGregor     (dmc270)
+//
+// Support for switch input, for the Tiva board, handles
+// up and down pushes
+//
+//**********************************************************
+
 #ifndef SWITCH_H_
 #define SWITCH_H_
 
 #include <stdint.h>
 #include <stdbool.h>
 
+// Enum for the differnt states of a switch imput 
 typedef enum {
     SWITCH_NO_CHANGE = 0,
     SWITCH_UP,
     SWITCH_DOWN
 } switch_state_t;
 
+// Struct for switch, contains current switch position,
+// new position, and whether the switch has changed
+// since last checked
 typedef struct
 {
     bool switch_position;
@@ -17,9 +32,21 @@ typedef struct
     bool switch_changed;
 } switch_t;
 
-
+//**********************************************************
+// initSwitch: Initialises Switch_1
+//**********************************************************
 void initSwitch(void);
+
+//**********************************************************
+// updateSwitch: Checks new switch position and sees
+// if switch_1's positions has changed
+//**********************************************************
 void updateSwitch(void);
+
+//**********************************************************
+// checkSwitch: Checks switch_1 and returns if it has not
+// changed since last check, and then its positon
+//**********************************************************
 switch_state_t checkSwitch(void);
 
-#endif
+#endif // SWITCH_H_
